ca5acae149833ff23a7882a2136aafbe84c0b540
fabio-zanchetta <61703434+fabio-zanchetta@users.noreply.github.com>
2020-03-31 16:41:41 +0200

    Update esercizio20200330.c

--- src/esercizio20200330.c
+++ src/esercizio20200330.c
@@ -18,20 +18,44 @@ int main(int argc, char *argv[]) {
 	n--;
 	printf("Il massimo valore di n per cui il risultato di fibonacci(n) è accurato è %lu",n);
 	
-	//B
+	//C
+	unsigned long *fibonacci_result;
+	unsigned long *fibonacci_result_copy;
+	
+	fibonacci_result  = fibonacci_array(39);
+	
+	fibonacci_result_copy = 
+	
+	
 	return 0;
 }
 
 
-unsigned long fibonacci(unsigned int n){
+/*unsigned long fibonacci(unsigned int n){
 	unsigned long f;
-	if(n = 0)
+	if(n == 0)
 		return 0;
-	else if(n = 1)
+	else if(n == 1)
 		return 1;
 	else
 		f = fibonacci(n-1) + fibonacci(n-2);
 	
+	return f;
+}*/
+
+unsigned long fibonacci(unsigned int n){
+	unsigned long f0 = 0, f1 = 1, f;
+	
+	if(n == 0)
+		return 0;
+	else if(n == 1)
+		return 1;
+	
+	for(int i = 2; i <= n; i++){
+		f = f0 + f1;
+		f0 = f1;
+		f1 = f;
+	}
 	return f;
 }
 
@@ -39,4 +63,12 @@ unsigned long fibonacci(unsigned int n){
 unsigned long * fibonacci_array(unsigned int n){
 	unsigned long *fib_array;
 	
-	fib_array = calloc(n+1, sizeof(unsigned long);
+	fib_array = calloc(n+1, sizeof(unsigned long));
+	fib_array[1] = 1;
+	
+	for(int i = 2; i <= n; i++){
+		fib_array[i] = fib_array[i-1] + fib_array[i-2];
+	}
+	
+	return fib_array;
+}
